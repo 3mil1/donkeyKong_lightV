70dad5771a65b8b0216afbe1971be222cfc82707
Emil <emil.varn@gmail.com>
2022-02-07 12:13:00 +0200

    falls into a hole (many holes)

--- Mario.mjs
+++ Mario.mjs
@@ -115,18 +115,21 @@ function jump() {
 
 function isCollide() {
     let platform = document.querySelectorAll(`.platform.floor${plumber.onFloor}`)
-    let falling = document.querySelector(`.hole`)
-    let fallingRect = falling.getBoundingClientRect()
+    let falling = document.querySelectorAll(`.hole`)
     let ladder = document.querySelectorAll(`.ladder.floor${plumber.onFloor}`)
     let marioRect = mario.getBoundingClientRect()
 
     // falls into a hole
-    if (marioRect.bottom === fallingRect.top
-        && marioRect.left >= fallingRect.left
-        && marioRect.right <= fallingRect.right
-    ) {
-        mario.style.top = parseInt(mario.style.top, 10) + 1 + 'px'
+    for (const hole of falling) {
+        let holeRect = hole.getBoundingClientRect()
+        if (marioRect.bottom === holeRect.top
+            && marioRect.left >= holeRect.left
+            && marioRect.right <= holeRect.right
+        ) {
+            mario.style.top = parseInt(mario.style.top, 10) + 1 + 'px'
+        }
     }
+
 
     // falling down from platform
     if (!(marioRect.right - 10 >= platform.item(0).getBoundingClientRect().left && (marioRect.left + 10) <= platform.item(platform.length - 1).getBoundingClientRect().right)) {
