b45f52cf687543552920f83ba9b7df96afc4557a
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-02-15 15:28:51 +0200

    stop barrel moving if game on pause

--- barrel.mjs
+++ barrel.mjs
@@ -1,4 +1,6 @@
 import {bump, plumber} from "./Mario.mjs";
+import gamePause from "./gamePause.mjs";
+import {pauseGame} from "./index.mjs";
 
 class Barrel {
     constructor() {
@@ -15,18 +17,21 @@ class Barrel {
 
     moving() {
         let i = 0;
-        // let x = Math.floor(Math.random() * 225 + 15) / 20;
-        // x = x * (Math.round(Math.random()) * 2 - 1);
+        let x = Math.floor(Math.random() * 225 + 15) / 20;
+        x = x * (Math.round(Math.random()) * 2 - 1);
         let gR = this.game.getBoundingClientRect();
         let iId = setInterval(() => {
             let wCurrent = this.barrel.offsetLeft;
             let hCurrent = this.barrel.offsetTop;
-            // wCurrent += x;
+            wCurrent += x;
             hCurrent += 5;
-            this.barrel.style.top = hCurrent + 'px';
-            // this.barrel.style.left = wCurrent + 'px';
-            this.barrel.classList.remove(...this.moves);
-            this.barrel.classList.add(this.moves[i % this.moves.length]);
+            if (!pauseGame.paused) {
+                this.barrel.style.top = hCurrent + 'px';
+                this.barrel.style.left = wCurrent + 'px';
+
+                this.barrel.classList.remove(...this.moves);
+                this.barrel.classList.add(this.moves[i % this.moves.length]);
+            }
             i++;
 
             if (this.barrel.offsetLeft < 0 || this.barrel.offsetLeft + 15 > gR.width || this.barrel.offsetTop + 15 > gR.bottom) {
@@ -38,36 +43,6 @@ class Barrel {
         this.barrel.dataset.intervalID = iId + '';
 
     }
-
-
-
-//     explode(barrelRect) {
-//         let explodedBarrel = document.createElement('div')
-//         explodedBarrel.classList.add('barrel', 'explode')
-//         let mario = document.getElementById('mario');
-//         let mRect = mario.getBoundingClientRect();
-//         explodedBarrel.style.left = mRect.left + 'px'
-//         explodedBarrel.style.top = mRect.top + 'px',
-//
-//
-//         console.log(explodedBarrel.style.left)
-//         console.log(explodedBarrel.style.top)
-//
-//         this.game.appendChild(explodedBarrel)
-//
-//         let x = 0
-//         let i = 0;
-//         let intervalID = setInterval(() => {
-//             explodedBarrel.classList.remove(...this.brakeMoves)
-//             explodedBarrel.classList.add(this.brakeMoves[i % this.brakeMoves.length])
-//             i++
-//
-//             if (++x === 5) {
-//                 explodedBarrel.remove()
-//                 window.clearInterval(intervalID)
-//             }
-//         }, 100);
-//     }
 }
 
 export default Barrel;
